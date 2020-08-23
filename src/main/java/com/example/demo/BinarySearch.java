8e8ca5514ad39d5b4459965b0ad12ac0130c6a42
SaiAshish9 <saiashish3760@gmail.com>
2020-08-23 15:25:10 +0530

    analysed autowiring cases

--- src/main/java/com/example/demo/BinarySearch.java
+++ src/main/java/com/example/demo/BinarySearch.java
@@ -7,27 +7,37 @@ import org.springframework.stereotype.Component;
 public class BinarySearch {
     
 	@Autowired
-    private SortAlgorithm sortAlgorithm;
-		
+	private SortAlgorithm bubbleSort;
+//    private SortAlgorithm sortAlgorithm;
+	
+//	private SortAlgorithm bubbleSort works without autowiring
+	
 //	sortAlgorithm is a dependency for binary search
-    
+//    mandatory dependencies -> constructor injection
+//	  optional dependencies -> setter injection
    
-	public BinarySearch(SortAlgorithm sortAlgorithm) {
-		super();
-		this.sortAlgorithm = sortAlgorithm;
-	}
+//	public BinarySearch(SortAlgorithm sortAlgorithm) {
+//		super();
+//		this.sortAlgorithm = sortAlgorithm;
+//	}
 
-
+	
+// setter injection and constructor injection or no setter,constructor
 
 	public int binarySearch(int[] numbers,int numberToSearch) {
 	
-		int[] sortedNumbers=sortAlgorithm.sort(numbers);
-        System.out.println(sortAlgorithm);
+		int[] sortedNumbers=bubbleSort.sort(numbers);
+        System.out.println(bubbleSort);
 //		linear search
 //		binary search loose coupling
 		
 		return 3;
 
 	}
+
+
+//	public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+//		this.sortAlgorithm = sortAlgorithm;
+//	}
 	
 }
