f03fb19db361aba27cf53867746265179a86eee4
SaiAshish9 <saiashish3760@gmail.com>
2020-08-23 12:12:11 +0530

    updated beans

--- src/main/java/com/example/demo/BinarySearch.java
+++ src/main/java/com/example/demo/BinarySearch.java
@@ -1,5 +1,33 @@
 package com.example.demo;
 
-public class BinarySearch {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class BinarySearch {
+    
+	@Autowired
+    private SortAlgorithm sortAlgorithm;
+		
+//	sortAlgorithm is a dependency for binary search
+    
+   
+	public BinarySearch(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+	}
+
+
+
+	public int binarySearch(int[] numbers,int numberToSearch) {
+	
+		int[] sortedNumbers=sortAlgorithm.sort(numbers);
+        System.out.println(sortAlgorithm);
+//		linear search
+//		binary search loose coupling
+		
+		return 3;
+
+	}
+	
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+// component scan 
+// automatically search packages and sub packages
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+//	    BinarySearch binarySearch=new BinarySearch(new BubbleSort());
+//	    BinarySearch binarySearch=new BinarySearch(new QuickSort());
+	    
+//	Application Context ->	QuickSort And BubbleSort Beans are managed by application context
+		
+		ApplicationContext applicationContext=SpringApplication.run(DemoApplication.class, args);	
+		BinarySearch binarySearch= applicationContext.getBean(BinarySearch.class);
+		int result=binarySearch.binarySearch(new int[] {12,4,6,8},4);
+	    System.out.println(result);
+	    
 	}
-
 }
+
+// What are beans? --> @component annotation
+// What are the dependencies of a bean? -> @autowired
+// Where to search for beans?
+// Dependency Management 
