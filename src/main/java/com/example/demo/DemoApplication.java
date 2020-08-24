da0f0c931d9b9a0372eaba332b9f0769c9e94956
SaiAshish9 <saiashish3760@gmail.com>
2020-08-24 16:50:55 +0530

    @Value And @PropertySource Added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,12 @@ package com.example.demo;
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.context.ApplicationContext;
-
+//import org.springframework.core.SpringVersion;
 import com.example.demo.basic.BinarySearch;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
+import org.springframework.core.SpringVersion;
 
 
 // spring-core where beans are defined
@@ -34,17 +35,36 @@ public class DemoApplication {
 	    
 //	Application Context ->	QuickSort And BubbleSort Beans are managed by application context
 //		ApplicationContext applicationContext=SpringApplication.run(DemoApplication.class, args);	
-		ApplicationContext applicationContext=
-				new AnnotationConfigApplicationContext(DemoApplication.class);
+		try(AnnotationConfigApplicationContext applicationContext=
+				new AnnotationConfigApplicationContext(DemoApplication.class)){
 		BinarySearch binarySearch= applicationContext.getBean(BinarySearch.class);
 		BinarySearch binarySearch1= applicationContext.getBean(BinarySearch.class);
 		int result=binarySearch.binarySearch(new int[] {12,4,6,8},4);
 	    System.out.println(binarySearch);
 	    System.out.println(binarySearch1);
-	}
+	    
+//	    System.out.println(SpringVersion.getVersion());
+	    
+//	    applicationContext.close();
+		}	}
 }
 
 // What are beans? --> @component annotation
 // What are the dependencies of a bean? -> @autowired
 // Where to search for beans?
 // Dependency Management 
+
+
+
+//<dependency>
+//<groupId>org.slf4j</groupId>
+//<artifactId>slf4j-api</artifactId>
+//</dependency>
+
+
+//<dependency>
+// <groupId>ch.qos.logback</groupId>
+// <artifactId>logback-classic</artifactId>
+//</dependency>
+
+
